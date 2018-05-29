b590e5be2b60017d8e7a0bcfb051fac8ae311684
Shorab Jamal <shorabjamal@hotmail.ca>
2018-05-29 11:59:52 -0400

    Initial commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = (props) => (
+    <Card>
+      <CardSection>
+        <View>
+        <Image style = {styles.thumbnailStyle} source={{ uri: props.album.thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+        <Text>{props.album.title}</Text>
+        <Text>{props.album.artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+  );
+
+
+  const styles = {
+    headerContentStyle:{
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+
+    thumbnailStyle: {
+      height: 50,
+      width: 50
+    }
+  };
+
+export default AlbumDetail;
